362660130d6145171d5822babde715e78c9172d1
PaulaMolinaRuiz <pa.molina@uniandes.edu.co>
2025-01-23 09:34:49 -0500

    Challenge1 O(n)

--- src/ChallengeOne.java
+++ src/ChallengeOne.java
@@ -11,11 +11,29 @@ public class ChallengeOne {
         // Filter digits greater than or equal to S
         for (Integer num : inputList) {
             StringBuilder newNum = new StringBuilder();
-            for (char digit : num.toString().toCharArray()) {
-                if (Character.getNumericValue(digit) < S) {
+
+            char[] digit = num.toString().toCharArray();
+            
+            if (num< S) {
+                newNum.append(digit);
+            }
+            else if(num > S){
+                
+                int digit0=Character.getNumericValue(digit[0]);
+                int digit1=Character.getNumericValue(digit[1]);
+
+                if(digit0 < S && digit1 < S){
                     newNum.append(digit);
                 }
+                else if(digit0 > S && digit1 < S){
+                    newNum.append(digit1);
+                }
+                else if(digit0 < S && digit1 > S)
+                {
+                    newNum.append(digit0);
+                }
             }
+
             if (newNum.length() > 0) {
                 filteredList.add(Integer.parseInt(newNum.toString()));
             }
