ac6078eb5803a35231adebe2fc4bef677fdbf9d0
PaulaMolinaRuiz <pa.molina@uniandes.edu.co>
2025-01-23 09:52:23 -0500

    Challenge two fixed

--- src/ChallengeTwo.java
+++ src/ChallengeTwo.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class ChallengeTwo {
 
@@ -19,9 +21,35 @@ public class ChallengeTwo {
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 5, 6, 8, 9};
         int S = 6;
+        int n = generateRandomN();
+
+        int[] array = generateRandomArray(n);
+        Arrays.sort(array); // Sorting the generated array
+        System.out.print("Generated Sorted Array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        
         List<Integer> result = sortedSquares(array, S);
-        System.out.println(result);
+        System.out.println("Result: " + result);
+    }
+
+    public static int generateRandomN() {
+        Random random = new Random();
+        return random.nextInt(100) + 1; // Generates a random number between 1 and 100
+    }
+
+    public static int[] generateRandomArray(int n) {
+        Random random = new Random();
+        int[] randomArray = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            // Generate random integers in the range -100 to 100
+            randomArray[i] = random.nextInt(201) - 100;
+        }
+        
+        return randomArray;
     }
 }
